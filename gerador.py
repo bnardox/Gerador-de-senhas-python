2028fc9d96c4173ee639e313babe38052238f999
Razzynt <eubernardoassis@gmail.com>
2021-05-15 19:11:21 -0300

    Base do projeto pronta

--- gerador.py
+++ gerador.py
@@ -1,2 +1,16 @@
 import random
 import string
+
+
+senha = ''
+symbols = '#@&!'
+numbers = '0123456789'
+
+group = string.ascii_letters + numbers + symbols
+
+size = int(input('Escolha um tamano: '))
+
+for p in range(size):
+    senha += random.choice(group)
+
+print(senha)
