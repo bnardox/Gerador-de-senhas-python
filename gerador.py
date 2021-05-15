f914459917583ebd7387202c576de94fbe91d06c
Razzynt <eubernardoassis@gmail.com>
2021-05-15 19:18:42 -0300

    Adicionando comentarios para o codigo base

--- gerador.py
+++ gerador.py
@@ -2,14 +2,19 @@ import random
 import string
 
 
+#Definindo algumas variavies importantes
 senha = ''
 symbols = '#@&!'
 
+#Concatenando as letras maiusculas e minusculas e números
 group = string.ascii_letters + string.digits + symbols
 
+#Input receber o tamanho da senha
 size = int(input('Escolha um tamano: '))
 
+#Gerando a senha de acordo com o tamanho
 for p in range(size):
     senha += random.choice(group)
 
+#Mostrando a senha
 print(senha)
